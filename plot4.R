8726a72d7f2b92fd69db92497be94b9bd636a571
MikeYowen <git@wyoming.isagoon.com>
2014-08-04 14:22:12 -0700

    Fix plot4.R

--- plot4.R
+++ plot4.R
@@ -4,7 +4,7 @@ data[,1] <- as.Date(data[,1], "%d/%m/%Y")
 data1 <- subset(data, data[,1] == as.Date("01/02/2007", "%d/%m/%Y"))
 data2 <- subset(data, data[,1] == as.Date("02/02/2007", "%d/%m/%Y"))
 data <-  rbind(data1, data2)
-data$DateTime <- strptime(paste(data$Date, data$Time), "%d/%m/%Y %H:%M:%S") 
+data$DateTime <- strptime(paste(data$Date, data$Time), "%Y-%m-%d %H:%M:%S") 
 
 png("plot4.png", width = 480, height = 480)
 par(mfrow=c(2,2))
