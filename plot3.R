b7ae540cd9b74f45c9f27bbb786e3f5332b13865
MikeYowen <git@wyoming.isagoon.com>
2014-08-04 14:20:14 -0700

    Updates

--- plot3.R
+++ plot3.R
@@ -4,7 +4,7 @@ data[,1] <- as.Date(data[,1], "%d/%m/%Y")
 data1 <- subset(data, data[,1] == as.Date("01/02/2007", "%d/%m/%Y"))
 data2 <- subset(data, data[,1] == as.Date("02/02/2007", "%d/%m/%Y"))
 data <-  rbind(data1, data2)
-data$DateTime <- strptime(paste(data$Date, data$Time), "%d/%m/%Y %H:%M:%S") 
+data$DateTime <- strptime(paste(data$Date, data$Time), "%Y-%m-%d %H:%M:%S") 
 
 png("plot3.png", width = 480, height = 480)
 plot(data[,10],as.numeric(as.character(data[,7])), type="l", xlab="", ylab="Energy sub metering")
